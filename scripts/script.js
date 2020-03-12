9d733fd75c4449439b9afef1cd278886c002154d
Lois Simpson <lsimpson667@gmail.com>
2020-03-12 10:31:29 -0400

    Finished show my love project

---
+++ scripts/script.js
@@ -0,0 +1,36 @@
+
+
+
+// The function that is used to show a message when the button is clicked.
+function saySomething() {
+    const name = prompt("What is your name?");
+
+    let color = prompt("What is your favorite color?");
+
+// this part of the function is based on user input
+if (color === "blue") {
+    alert("We are a match made in heaven!");
+} else if (color === "yellow") {
+    alert("You are my soulmate!");
+} else if (color === "green") {
+    alert("Let's be together!");
+} else {
+    alert("Not a match.");
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
